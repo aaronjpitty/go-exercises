de2f90aae9967d71cad81f41ef0243dd18ebb319
Aaron Pitty <alphalos10@gmail.com>
2020-04-27 20:09:40 +0100

    Various updates

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 )
 
+// ShareWith function. If string not empty, use one phrase, else, fall back to my name.
 func ShareWith(name string) string {
 
 	if len(name) > 0 {
-		return fmt.Sprintf("One for %v, one for me.", name)
-	} else {
-		return "One for you, one for me."
+		return fmt.Sprintf("One for %s, one for me.", name)
+	} else if name == "" {
+		name = "aaron pitty"
 	}
+
+	return "One for you, one for me."
 
 }
